5ddcbf47af146e945ea7823827e509a54f4cfea1
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-17 16:17:04 -0300

    Cria account repository

---
+++ AODevBlog.Repository/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using AODevBlog.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AODevBlog.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, 
+            CancellationToken cancelationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername,
+            CancellationToken cancellationToken);
+
+    }
+}
