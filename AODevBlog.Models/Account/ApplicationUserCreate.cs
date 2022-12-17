f4589a10815b78acb138df54c8ccf678a457aaf2
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-17 14:51:53 -0300

    Cria model da conta

---
+++ AODevBlog.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AODevBlog.Models.Account
+{
+    public class ApplicationUserCreate : ApplicationUserLogin
+    {
+        [MinLength(10, ErrorMessage = "Precisa ter pelo menos 10-30 caracteres")]
+        [MaxLength(30, ErrorMessage = "Precisa ter pelo menos 10-30 caracteres")]
+        public string Fullname { get; set; }
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [MaxLength(30, ErrorMessage = "Máximo de 30 caracteres permitido")]
+        [EmailAddress(ErrorMessage = "Email com formato inválido")]
+        public string Email { get; set; }
+    }
+}
---
+++ AODevBlog.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AODevBlog.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        [MinLength(5, ErrorMessage = "Precisa ter pelo menos 5-20 caracteres")]
+        [MaxLength(20, ErrorMessage = "Precisa ter pelo menos 5-20 caracteres")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        [MinLength(10, ErrorMessage = "Precisa ter pelo menos 10-50 caracteres")]
+        [MaxLength(50, ErrorMessage = "Precisa ter pelo menos 10-50 caracteres")]
+        public string Password { get; set; }
+    }
+}
