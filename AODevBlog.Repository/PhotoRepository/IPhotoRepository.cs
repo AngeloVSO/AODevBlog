288b08e04b93ec59402bcda4a808c596ab34d323
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-17 16:56:04 -0300

    Cria repository de account e de photo

---
+++ AODevBlog.Repository/PhotoRepository/IPhotoRepository.cs
@@ -0,0 +1,16 @@
+﻿using AODevBlog.Models.Photo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AODevBlog.Repository.PhotoRepository
+{
+    public interface IPhotoRepository
+    {
+        public Task<Photo> InsertAsync(PhotoCreate photoCreate, int applicationuUserId);
+        public Task<Photo> GetAsync(int photoId);
+        public Task<List<Photo>> GetAllByUserIdAsync(int applicationuUserId);
+        public Task<int> DeleteAsync(int photoId);
+    }
+}
