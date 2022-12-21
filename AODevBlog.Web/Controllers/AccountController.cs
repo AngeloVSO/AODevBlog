63bb006cab2e3d7f2c1a1eed916ddfcb57c71cc5
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-21 16:59:28 -0300

    Cria account e photo controller

---
+++ AODevBlog.Web/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using AODevBlog.Models.Account;
+using AODevBlog.Services.TokenService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AODevBlog.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ITokenService _tokeService;
+        private readonly UserManager<ApplicationUserIdentity> _userManager;
+        private readonly SignInManager<ApplicationUserIdentity> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUserIdentity> signInManager, UserManager<ApplicationUserIdentity> userManager, ITokenService tokeService)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _tokeService = tokeService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ApplicationUser>> Register(ApplicationUserCreate applicationUserCreate)
+        {
+            var applicationUserIdentity = new ApplicationUserIdentity
+            {
+                Username = applicationUserCreate.Username,
+                Email = applicationUserCreate.Email,
+                Fullname = applicationUserCreate.Fullname
+            };
+
+            var result = await _userManager.CreateAsync(applicationUserIdentity, applicationUserCreate.Password);
+
+            if (result.Succeeded)
+            {
+                var user = new ApplicationUser()
+                {
+                    ApplicationUserId = applicationUserIdentity.ApplicationUserId,
+                    UserName = applicationUserIdentity.Username,
+                    Email = applicationUserIdentity.Email,
+                    FullName = applicationUserIdentity.Fullname,
+                    Token = _tokeService.CreateToken(applicationUserIdentity)
+                };
+
+                return Created("user", user);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApplicationUser>> Login(ApplicationUserLogin applicationUserLogin)
+        {
+            var applicationUserIdentity = await _userManager.FindByNameAsync(applicationUserLogin.Username);
+
+            if (applicationUserIdentity != null)
+            {
+                var result = await _signInManager.CheckPasswordSignInAsync(
+                    applicationUserIdentity, 
+                    applicationUserLogin.Password, false);
+
+                if (result.Succeeded)
+                {
+                    var applicationUser = new ApplicationUser
+                    {
+                        ApplicationUserId = applicationUserIdentity.ApplicationUserId,
+                        UserName = applicationUserIdentity.Username,
+                        Email = applicationUserIdentity.Email,
+                        FullName = applicationUserIdentity.Fullname,
+                        Token = _tokeService.CreateToken(applicationUserIdentity)
+                    };
+
+                    return Ok(applicationUser);
+                }
+            }
+
+            return BadRequest("Invalid login attempt.");
+        }
+    }
+}
---
+++ AODevBlog.Web/Controllers/PhotoController.cs
@@ -0,0 +1,103 @@
+﻿using AODevBlog.Models.Photo;
+using AODevBlog.Repository.BlogRepository;
+using AODevBlog.Repository.PhotoRepository;
+using AODevBlog.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AODevBlog.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoRepository _photoRepository;
+        private readonly IBlogRepository _blogRepository;
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(IPhotoService photoService, IPhotoRepository photoRepository, IBlogRepository blogRepository)
+        {
+            _photoService = photoService;
+            _photoRepository = photoRepository;
+            _blogRepository = blogRepository;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Photo>> UploadPhoto(IFormFile file)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var uploadResult = await _photoService.AddPhotAsync(file);
+
+            if (uploadResult.Error != null) return BadRequest(uploadResult.Error.Message);
+
+            var photoCreate = new PhotoCreate
+            {
+                PublicId = uploadResult.PublicId,
+                ImageUrl = uploadResult.SecureUrl.AbsoluteUri,
+                Description = file.FileName
+            };
+
+            var photo = await _photoRepository.InsertAsync(photoCreate, applicationUserId);
+
+            return Ok(photo);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<List<Photo>>> GetByApplicationUserId()
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var photos = await _photoRepository.GetAllByUserIdAsync(applicationUserId);
+
+            return Ok(photos);
+        }
+
+        [HttpGet("{photoId}")]
+        public async Task<ActionResult<Photo>> Get(int photoId)
+        {
+            var photo = await _photoRepository.GetAsync(photoId);
+
+            return Ok(photo);
+        }
+
+        [Authorize]
+        [HttpDelete("{photoId}")]
+        public async Task<ActionResult<int>> Delete(int photoId)
+        {
+            int applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var foundPhoto = await _photoRepository.GetAsync(photoId);
+
+            if (foundPhoto != null && foundPhoto.ApplicationUserId == applicationUserId)
+            {
+                var blogs = await _blogRepository.GetAllByUserIdAsync(applicationUserId);
+
+                var usedInBlog = blogs.Any(b => b.Photoid == photoId);
+
+                if (usedInBlog) return BadRequest("Cannot remove photo as it is being used in published blog(s).");
+
+                var deleteResultService = await _photoService.DeletePhotoAsync(foundPhoto.PublicId);
+
+                if (deleteResultService.Error != null) return BadRequest(deleteResultService.Error.Message);
+
+                var deleteResultRepository = await _photoRepository.DeleteAsync(foundPhoto.PhotoId);
+
+                return Ok(deleteResultRepository);
+            }
+            else
+            {
+                return BadRequest("Photo was not uploaded by the current user.");
+            }
+        }
+
+    }
+}
