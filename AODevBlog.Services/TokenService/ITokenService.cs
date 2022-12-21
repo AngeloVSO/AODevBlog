b3731fe9ad07438440574d434a3abea9798d6e48
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-20 21:15:54 -0300

    Cria token service e organiza arquivos de dervice

---
+++ AODevBlog.Services/TokenService/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using AODevBlog.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AODevBlog.Services.TokenService
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
---
+++ AODevBlog.Services/TokenService/TokenService.cs
@@ -0,0 +1,42 @@
+﻿using AODevBlog.Models.Account;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AODevBlog.Services.TokenService
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["jwt:Key"]));
+            _issuer = config["Jwt:Issuer"];
+        }
+        public string CreateToken(ApplicationUserIdentity user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username),
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                _issuer,
+                _issuer,
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
