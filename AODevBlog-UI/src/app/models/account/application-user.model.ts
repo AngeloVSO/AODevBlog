c4500091718bb3fbdfc0dc4e6ea5281ce39412fd
AngeloVSO <angelo_odwyer@hotmail.com>
2022-12-24 16:07:37 -0300

    Ajuste username e fullname do model

--- AODevBlog-UI/src/app/models/account/application-user.model.ts
+++ AODevBlog-UI/src/app/models/account/application-user.model.ts
@@ -1,8 +1,8 @@
 export class ApplicationUser {
   constructor(
     public applicationUserId: number,
-    public username: string,
-    public fullname: string,
+    public userName: string,
+    public fullName: string,
     public email: string,
     public token: string
   ) {}
