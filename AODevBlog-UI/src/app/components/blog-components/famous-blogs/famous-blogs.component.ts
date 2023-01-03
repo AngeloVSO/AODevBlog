e99bbf88e68b79fab94cdb3ff4622827249a2156
AngeloVSO <angelo_odwyer@hotmail.com>
2023-01-03 19:56:30 -0300

    Cria famous component

--- AODevBlog-UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
+++ AODevBlog-UI/src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
@@ -1,15 +1,19 @@
+import { BlogService } from 'src/app/services/blog.service';
+import { Blog } from './../../../models/blog/blog.model';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-famous-blogs',
   templateUrl: './famous-blogs.component.html',
-  styleUrls: ['./famous-blogs.component.css']
+  styleUrls: ['./famous-blogs.component.css'],
 })
 export class FamousBlogsComponent implements OnInit {
+  famousBlogs$!: Observable<Blog[]>;
 
-  constructor() { }
+  constructor(private blogService: BlogService) {}
 
   ngOnInit(): void {
+    this.famousBlogs$ = this.blogService.getMostFamous();
   }
-
 }
